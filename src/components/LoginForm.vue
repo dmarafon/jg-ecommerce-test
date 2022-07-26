34a711d3d8d9f33a0e480843fbae7b6b82a67ac3
Daniell Marafon <daniellmarafon@gmail.com>
2022-07-27 01:12:47 +0200

    builded formStore and added types for login

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="login__container">
+    <div>
+      <h1>Sign Up</h1>
+    </div>
+    <form noValidate autoComplete="off" @submit.prevent="handleSubmit">
+      <div class="login__input--email">
+        <label for="email">Email Address</label>
+        <input
+          id="email"
+          v-model="storeLogin.email"
+          type="email"
+          placeholder=" "
+        />
+      </div>
+      <div class="login__input--password">
+        <label for="password">Password</label>
+        <input
+          id="password"
+          v-model="storeLogin.password"
+          type="password"
+          placeholder=" "
+        />
+      </div>
+      <div class="login__input--button">
+        <button type="submit">Log In</button>
+      </div>
+    </form>
+  </section>
+</template>
