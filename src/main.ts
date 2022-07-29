0da7cb21578946f5114379ef49324902337b0f9a
Daniell Marafon <daniellmarafon@gmail.com>
2022-07-29 14:24:36 +0200

    small fix in header css

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,9 @@ import "./style.css";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 
 createApp(App).use(createPinia()).use(router).mount("#app");
