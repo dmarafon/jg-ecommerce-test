d80cdc1783ed1213b60342c40239aa146d06f529
Daniell Marafon <daniellmarafon@gmail.com>
2022-07-29 13:24:59 +0200

    fixed header css

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,5 @@ import "./style.css";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-createApp(App)
-  .use(createPinia())
-  .use(piniaPluginPersistedstate)
-  .use(router)
-  .mount("#app");
+createApp(App).use(createPinia()).use(router).mount("#app");
