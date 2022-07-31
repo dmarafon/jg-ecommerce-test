fbed87249bdc56fc5681a7ef343eccdf10c24143
Daniell Marafon <daniellmarafon@gmail.com>
2022-07-31 15:14:07 +0200

    first draft of product card html and css completed

---
+++ src/components/Product/Product.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <li className="product__list" key="">
+    <div className="product__container">
+      <img
+        className="product__image"
+        src=""
+        alt=""
+        width="417.44"
+        height="465"
+      />
+      <div className="product__text">
+        <h2 className="product__text--author">""</h2>
+        <h3 className="product__text--title">""</h3>
+        <p className="product__text--price">""</p>
+      </div>
+    </div>
+  </li>
+</template>
+
+<style scoped src="./ProductStyle.css"></style>
