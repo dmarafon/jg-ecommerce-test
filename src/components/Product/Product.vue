9c3487aacd89f876215b1791b916f57902276dd4
Daniell Marafon <daniellmarafon@gmail.com>
2022-07-31 16:13:06 +0200

    fixed all css for product

--- src/components/Product/Product.vue
+++ src/components/Product/Product.vue
@@ -10,21 +10,23 @@ const categoryToUpperCase: string = props.product.category.toUpperCase();
 </script>
 
 <template>
-  <li className="product__list">
-    <div className="product__container">
-      <img
-        className="product__image"
-        :src="props.product.images[0]"
-        alt="marketplace product image"
-        width="417.44"
-        height="465"
-      />
-      <div className="product__text">
-        <h2 className="product__text--category">
-          {{ categoryToUpperCase }}
-        </h2>
-        <h3 className="product__text--title">{{ props?.product?.title }}</h3>
-        <p className="product__text--price">€{{ props?.product?.price }}</p>
+  <li class="product__list">
+    <div class="product__container">
+      <div class="product__card">
+        <img
+          class="product__image"
+          :src="props.product.images[0]"
+          alt="marketplace product image"
+          width="417.44"
+          height="465"
+        />
+        <div class="product__text">
+          <h2 class="product__text--category">
+            {{ categoryToUpperCase }}
+          </h2>
+          <h3 class="product__text--title">{{ props?.product?.title }}</h3>
+          <p class="product__text--price">€{{ props?.product?.price }}</p>
+        </div>
       </div>
     </div>
   </li>
