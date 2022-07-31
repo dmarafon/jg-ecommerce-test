7189e49e371a8742652b1b4739413621c063ef23
Daniell Marafon <daniellmarafon@gmail.com>
2022-07-31 15:40:44 +0200

    first draft working product component

--- src/components/Product/Product.vue
+++ src/components/Product/Product.vue
@@ -1,19 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps<{
+  product: {
+    title?: string;
+    thumbnail?: string;
+    images?: string[];
+    price?: number;
+    category: string;
+  };
+}>();
+</script>
 
 <template>
   <li className="product__list" key="">
     <div className="product__container">
       <img
         className="product__image"
-        src=""
-        alt=""
+        :src="props.product.images[0]"
+        alt="marketplace product image"
         width="417.44"
         height="465"
       />
       <div className="product__text">
-        <h2 className="product__text--author">""</h2>
-        <h3 className="product__text--title">""</h3>
-        <p className="product__text--price">""</p>
+        <h2 className="product__text--category">
+          {{ props?.product?.category }}
+        </h2>
+        <h3 className="product__text--title">{{ props?.product?.title }}</h3>
+        <p className="product__text--price">€{{ props?.product?.price }}</p>
       </div>
     </div>
   </li>
