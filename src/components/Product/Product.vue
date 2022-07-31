a50f5b6a1bbf2fe9e78f9b6a47c0a0811fe926a0
Daniell Marafon <daniellmarafon@gmail.com>
2022-07-31 16:09:54 +0200

    fixed product component typescript

--- src/components/Product/Product.vue
+++ src/components/Product/Product.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
-const props = defineProps<{
-  product: {
-    title?: string;
-    thumbnail?: string;
-    images?: string[];
-    price?: number;
-    category: string;
-  };
-}>();
+interface Props {
+  product: any;
+}
+const props = withDefaults(defineProps<Props>(), {
+  product: () => [],
+});
+
+const categoryToUpperCase: string = props.product.category.toUpperCase();
 </script>
 
 <template>
-  <li className="product__list" key="">
+  <li className="product__list">
     <div className="product__container">
       <img
         className="product__image"
@@ -22,7 +21,7 @@ const props = defineProps<{
       />
       <div className="product__text">
         <h2 className="product__text--category">
-          {{ props?.product?.category }}
+          {{ categoryToUpperCase }}
         </h2>
         <h3 className="product__text--title">{{ props?.product?.title }}</h3>
         <p className="product__text--price">€{{ props?.product?.price }}</p>
