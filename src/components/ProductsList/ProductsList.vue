93946a609e071ae3d5d57ce274fa2e051336215e
Daniell Marafon <daniellmarafon@gmail.com>
2022-07-31 14:52:00 +0200

    fixed click navigation conditionalling rendering

--- src/components/ProductsList/ProductsList.vue
+++ src/components/ProductsList/ProductsList.vue
@@ -170,9 +170,8 @@ const sortZtoA = () => {
     </ul>
     <div className="products__navigate--container">
       <svg
-        v-if="Number(page) >= 1"
         data-testid="back-button"
-        @click="navigateBackwards"
+        v-on="Number(page) > 1 ? { click: navigateBackwards } : {}"
         :class="[
           Number(page) <= 1 ? deactivateNavigateClass : activeNavigateLeftClass,
         ]"
@@ -180,6 +179,7 @@ const sortZtoA = () => {
         viewBox="0 0 448 512"
       >
         <path
+          v-if="Number(page) > 1"
           fillOpacity="0"
           d="M447.1 256C447.1 273.7 433.7 288 416 288H109.3l105.4 105.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H416C433.7 224 447.1 238.3 447.1 256z"
         />
@@ -189,8 +189,7 @@ const sortZtoA = () => {
         {{ page }}/ {{ totalPages }}
       </p>
       <svg
-        v-if="Number(page) !== totalPages"
-        @click="navigateForward"
+        v-on="Number(page) !== totalPages ? { click: navigateForward } : {}"
         :class="[
           Number(page) === totalPages
             ? deactivateNavigateClass
@@ -201,6 +200,7 @@ const sortZtoA = () => {
         viewBox="0 0 448 512"
       >
         <path
+          v-if="Number(page) !== totalPages"
           fillOpacity="0"
           d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z"
         />
