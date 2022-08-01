8f726a372fe9ccf345adc4e0725bfb06523a2a73
Daniell Marafon <daniellmarafon@gmail.com>
2022-08-01 19:27:28 +0200

    finished building not found component

--- src/components/NotFound/NotFound.vue
+++ src/components/NotFound/NotFound.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
-
-<template></template>
+<template>
+  <section class="not-found__container">
+    <h2 class="not-found__title">404</h2>
+    <p class="not-found__text--line-first">PAGE NOT</p>
+    <p class="not-found__text--line-second">FOUND</p>
+  </section>
+</template>
 
 <style scoped src="./NotFoundStyle.css"></style>
