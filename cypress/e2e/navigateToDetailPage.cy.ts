2c6689d73cc905cb390791d15517286f189da21e
Daniell Marafon <daniellmarafon@gmail.com>
2022-08-02 13:25:58 +0200

    Adding test for cart drop item in cypress

--- cypress/e2e/navigateToDetailPage.cy.ts
+++ cypress/e2e/navigateToDetailPage.cy.ts
@@ -3,7 +3,7 @@ describe("Given a Products Page ", () => {
     const email: string = "test@test.com";
     const password: string = "12345";
 
-    it("Then it will render a detail page", () => {
+    it("Then it will render a detail page with all the items details text", () => {
       cy.visit("/");
 
       cy.get("#email")
@@ -22,7 +22,21 @@ describe("Given a Products Page ", () => {
 
       cy.wait(500);
 
-      cy.get(".detail__paragraph--special").should("contains.text");
+      cy.get(".detail__list")
+        .eq(0)
+        .should("contains.text", "Customers Rating: 4.69");
+
+      cy.get(".detail__list")
+        .eq(1)
+        .should("contains.text", " How Many Items in Stock? 94");
+
+      cy.get(".detail__list").eq(2).should("contains.text", "Brand: Apple");
+
+      cy.get(".detail__list").eq(3).should("contains.text", "Price: € 549");
+
+      cy.get(".detail__list")
+        .eq(4)
+        .should("contains.text", "What You Save? 12.96%");
     });
   });
 });
